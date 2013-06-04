bb1fd29c83cfa77bb791ada9529ff370456aea77
JJin <308366504@qq.com>
2013-06-04 21:00:35 +0800

    after studying errors

--- server/publications.js
+++ server/publications.js
@@ -1,3 +1,20 @@
 Meteor.publish('posts', function() {
 	return Posts.find();
 });
+
+Meteor.publish('comments', function() {
+	return Comments.find();
+});
+
+
+/*Publishing Partial Collections*/
+Meteor.publish('somePosts', function(){
+	return Posts.find({'author': 'Tom Coleman'});
+});
+
+/*Publishing Partial Properties*/
+Meteor.publish('allPosts', function(){
+	return Posts.find({}, {fields: {
+		date: false
+	}});
+});
